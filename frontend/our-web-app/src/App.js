d2f6a9907fee5450b3415434f299a25fcffd3e60
Lena Ptak <lenkaptak@gmail.com>
2022-11-20 22:12:37 +0100

    [#6] frontend: Saving files from D&D with FastApi

--- frontend/our-web-app/src/App.js
+++ frontend/our-web-app/src/App.js
@@ -1,14 +1,16 @@
-import React from "react"
-import DragDropFile from "./components/js/DragDropFile"
-
+import React from "react";
+import DragDropFile from "./components/js/DragDropFile";
 
 function App() {
   return (
     <div className="page">
-      <h1>React Drag & Drop</h1>
-      <DragDropFile />
-     </div>
-    )
+      <div className="header">
+        <h1>DOCUMENTS ANONYMISATION</h1>
+        <h5>Upload the files where you want to wipe sensitive data</h5>
+        <DragDropFile />
+      </div>
+    </div>
+  );
 }
 
 export default App;
