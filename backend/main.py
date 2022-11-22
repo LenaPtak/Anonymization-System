662e1fd53b77cd512e81ec18f7b6529560b79764
Lena Ptak <lenkaptak@gmail.com>
2022-11-22 11:28:51 +0100

    [#26] frontend: Download file from we (first try)

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.responses import FileResponse
 from fastapi import status, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 import os
 from pdf import PDF
@@ -62,6 +63,29 @@ async def main():
         </body>
     """
     return HTMLResponse(content=content)
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+"""
+Endpoint do ściągania plików ze strony.
+"""
+@app.get("/download/{filename}")
+async def download_file(filename: str):
+    file_path = f"files/{filename}"
+    return FileResponse(file_path, media_type='application/pdf', filename=file_path)
 
 
 if __name__ == "__main__":
--- frontend/our-web-app/src/App.js
+++ frontend/our-web-app/src/App.js
@@ -1,12 +1,13 @@
 import React from "react"
 import DragDropFile from "./components/js/DragDropFile"
-
+import DownloadFile from "./components/js/DownloadFile"
 
 function App() {
   return (
     <div className="page">
       <h1>React Drag & Drop</h1>
       <DragDropFile />
+      <DownloadFile />
      </div>
     )
 }
