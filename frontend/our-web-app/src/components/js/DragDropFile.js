711ab2b409b88c804c00eca398874d546adab162
Tomasz Pieścikowski <tomasz.piescikowski@nethone.com>
2022-11-22 21:01:01 +0100

    Fix ports react -> fastapi

--- frontend/our-web-app/src/components/js/DragDropFile.js
+++ frontend/our-web-app/src/components/js/DragDropFile.js
@@ -47,7 +47,7 @@ export default function DragDropFile() {
       body: formData,
     };
 
-    fetch("http://127.0.0.1:8000/upload", requestOptions).then((response) =>
+    fetch("http://127.0.0.1:9876/upload", requestOptions).then((response) =>
       response.json()
     );
   };
