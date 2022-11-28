31f39ac9cf7787971ff9d8ca6d1833c8fc8bdf12
Lena Ptak <lenkaptak@gmail.com>
2022-11-28 18:03:11 +0100

    [#49] frontend: styling homepage

--- frontend/our-web-app/src/components/js/homepage/Content.js
+++ frontend/our-web-app/src/components/js/homepage/Content.js
@@ -3,12 +3,15 @@ import "../../css/homepage/Content.css";
 import DragDropFile from "../DragDropFile";
 import DownloadFile from "../DownloadFile";
 import Invite from "./Invite";
+import Possibilities from "./Possibilities";
+
 
 
 export default function Navbar() {
   return (
     <div className="content">
       <Invite />
+      <Possibilities />
       <DownloadFile />
       <DragDropFile />
     </div>
--- frontend/our-web-app/src/components/js/homepage/Invite.js
+++ frontend/our-web-app/src/components/js/homepage/Invite.js
@@ -6,7 +6,8 @@ export default function Invite() {
     <div className="invite d-flex justify-content-center">
       <div className="invite__text">
         <h1 className="invite__tittle">WE MAKE ANONYMIZATION SIMPLE.</h1>
-        <h5 className="invite__under-tittle">Thanks to our tools Your data can be safe</h5>
+        <h5 className="invite__under-tittle">Thanks to our tools Your data can be safe.</h5>
+        <button className="invite__btn">Let's anonymize some documents</button>
       </div>
       <img className="invite__photo" src="..\images\homepage1.svg" alt="" />
     </div>
