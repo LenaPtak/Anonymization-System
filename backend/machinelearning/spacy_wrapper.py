22308a27053ea73788983f66538befe2f6daa072
Nastiuszka <anastasiia.trubchaninova@student.put.poznan.pl>
2022-12-05 23:23:16 +0100

    add repleaceing names in process results in spacy wrapper class

--- backend/machinelearning/spacy_wrapper.py
+++ backend/machinelearning/spacy_wrapper.py
@@ -1,3 +1,4 @@
+import re
 import spacy
 
 # TODO(Jan): Make machinelearning a module
@@ -18,18 +19,29 @@ class SpacyWrapper(Wrapper):
         else:
             raise NotImplementedError
         
+        super().__init__(weights_path)
+        
     def model(self, data):
         return self.model(data)
 
     def process_results(self, results, data):
         # tutaj ma zamazać tylko całe imiona i nazwiska z results.ents
-        pass
+        
+        name_pattern = re.compile(r"[A-Z][a-z]+\s+[A-Z][a-z]+")
+        replaced_data = data
+
+        for result in results:
+            if result.label_ == 'persName' and name_pattern.fullmatch(result.text) is not None:
+                replacemnt = 'X' * len(result.text)
+                replaced_data = replaced_data.replace(result.text, replacemnt)
+        
+        return replaced_data
 
 
 
 if __name__ == "__main__":
     sp = SpacyWrapper()
-    a = "Zdanie testowe Jana Kowalskiego"
+    a = "Jan Bylicki zrobil fajny opis jak dziala NLP na jego Linuksie i to calkiem niezle smiga i wyslal do Pani Anastasii Trubchaninovej i Tomasza P. na ares tomaszp1234@gmail.com wraz z numerem AL47 2121 1009 0000 0002 3569 87411"
     res = sp.model(a)
-    print(res)
-    sp.process_results(res, a)
+    result = sp.process_results(res.ents, a)
+    print(result)
