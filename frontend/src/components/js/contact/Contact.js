890f736c2b936872903bba9a5b52a447290d81c9
Lena Ptak <lenkaptak@gmail.com>
2022-12-14 18:21:24 +0100

    [#62] frontend: contact

--- frontend/src/components/js/contact/Contact.js
+++ frontend/src/components/js/contact/Contact.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Header from "../homepage/Header";
 import Footer from "../homepage/Footer";
+import "../../css/contact/Contact.css";
+
 
 export default function Contact() {
   return (
     <div>
       <Header />
-      <h1 style={{ margin: "15rem" }}>Contact us</h1>
+      <div className="contact__content row">
+        <div className="contact__container col">
+          <h1 className="contact__tittle">Contact</h1>
+          <div className="contact__description">
+            <li>lena.ptak@student.put.poznan.pl</li>
+            <li>tomasz.piescikowski@student.put.poznan.pl</li>
+            <li>anastasiia.trubchaninova@student.put.poznan.pl</li>
+            <li>jan.bylicki@student.put.poznan.pl</li>
+          </div>
+          <div className="contact__invite">
+            Please feel free to contact us if you have any questions.
+          </div>
+        </div>
+        <div className="contact__container col">
+          <img className="contact__logo" src="..\images\logoPP.png" alt="" />
+        </div>
+      </div>
       <Footer />
     </div>
   );
