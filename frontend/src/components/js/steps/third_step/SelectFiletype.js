b2547f0caf12eab99c86fab23c3beb3c1f7e90df
Lena Ptak <lenkaptak@gmail.com>
2022-12-28 19:25:45 +0100

    [#77] frontend: select filetype btn (third step)

--- frontend/src/components/js/steps/third_step/SelectFiletype.js
+++ frontend/src/components/js/steps/third_step/SelectFiletype.js
@@ -1,41 +1,16 @@
 import React from "react";
 import "../../../css/steps/third_step/SelectFiletype.css";
+import AllFilesType from "./AllFilesType";
 import Scrollable from "./Scrollable";
 
 export default function SelectFiletype() {
   return (
     <div className="row g-3">
-      <div className="col search">
+      <div className="col select-filetype__search">
         <Scrollable />
       </div>
-      <div class="col all-files">
-        <label class="form-label">
-          Choose your final filetype for all files:
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="jpg" />
-            <label class="form-check-label" for="jpg">
-              .jpg
-            </label>
-          </div>
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="pdf" />
-            <label class="form-check-label" for="pdf">
-              .pdf
-            </label>
-          </div>
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="txt" />
-            <label class="form-check-label" for="txt">
-              .txt
-            </label>
-          </div>
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="png" />
-            <label class="form-check-label" for="png">
-              .png
-            </label>
-          </div>
-        </label>
+      <div class="col select-filetype__all-files">
+        <AllFilesType />
       </div>
     </div>
   );
