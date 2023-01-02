89def96e1434126f94b0f0e7471d4e5378077a54
czarnobylu <jbylicki@hotmail.com>
2023-01-02 23:39:59 +0100

    [23] easyocr_wrapper: Added rendering and a test image
    Signed-off-by: czarnobylu <jbylicki@hotmail.com>

--- backend/machinelearning/easyocr_wrapper.py
+++ backend/machinelearning/easyocr_wrapper.py
@@ -1,6 +1,8 @@
 import easyocr
 from typing import List
 import cv2
+import numpy as np
+from PIL import Image
 
 # TODO(Jan): Make machinelearning a module
 
@@ -33,14 +35,33 @@ class EasyOCRWrapper(Wrapper):
     def model(self, data):
         return self.ocr_model.readtext(data)
 
+    def preprocess_results(self, results, data):
+        for box, text, p in results:
+            if p > 0.75:
+                top_l, top_r, bot_r, bot_l = box
+                print("[INFO] {:.4f}: {}".format(p, text))
+                top_l = (int(top_l[0]), int(top_l[1]))
+                top_r = (int(top_r[0]), int(top_r[1]))
+                bot_r = (int(bot_r[0]), int(bot_r[1]))
+                bot_l = (int(bot_l[0]), int(bot_l[1]))
+                text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+                cv2.rectangle(image, top_l, bot_r, (0, 255, 0), 2)
+                cv2.putText(
+                    image,
+                    text,
+                    (top_l[0], top_l[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2
+                )
+        im_pil = Image.fromarray(image)
+        im_pil.show()
+
 
 if __name__ == "__main__":
     import os
     eo = EasyOCRWrapper(None)
 
-    image = cv2.imread(os.getenv("HOME")+'/img.png')
+    image = cv2.imread(str(Path(__file__).parent / "./ocr_torture_test.png"))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = eo.model(image)
-    for i in results:
-        print(i)
+    eo.preprocess_results(results, image)
     # image = eo.process_results(results, image)
